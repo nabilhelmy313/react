a715b07574b24765301ae3aadd1f23dc124f3276
Nabil Helmy <NabilHelmy313@gmail.com>
2020-04-01 16:51:49 +0200

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,10 +13,12 @@ const minLength = (len) => (val) => val && (val.length >= len);
 class CommentForm extends Component{
   constructor(props) {
       super(props);
-      this.state = {
-          isModalOepn:false
-      }
+        this.state = {
+            isModalOepn:false
+        }
       this.toggleModal = this.toggleModal.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+        
   }
 
   toggleModal() {
@@ -26,8 +28,8 @@ class CommentForm extends Component{
         
   }
   handleSubmit(values) {
-      console.log('Current State is: ' + JSON.stringify(values));
-      alert('Current State is: ' + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
   render() {
       return (
@@ -112,7 +114,7 @@ class CommentForm extends Component{
       );
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments},addComment,dishId) {
       return comments === null ? (
         <div></div>
       ) : (
@@ -128,7 +130,7 @@ class CommentForm extends Component{
             }
           </ul>
 
-          <CommentForm/>  
+          <CommentForm dishId={dishId} addComment={addComment}/>  
         </div>
       );
     }
@@ -153,7 +155,10 @@ class CommentForm extends Component{
                 <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.comments} />
+              <RenderComments comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id}
+              />
             </div>
           </div>
         </div>
