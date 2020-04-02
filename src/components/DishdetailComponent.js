37dd6b9cf331824c4c847d38a8a2b13b1259f40a
Nabil Helmy <NabilHelmy313@gmail.com>
2020-04-02 14:36:49 +0200

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,8 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, Breadcrumb, BreadcrumbItem,
   Button,Modal,ModalHeader,ModalBody,Row,Col,Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors, Field } from 'react-redux-form';  
+import { Control, LocalForm, Errors, Field } from 'react-redux-form';
+import { Loading } from './LoadingComponent'  
 
     
 const required = (val) => val && val.length;
@@ -17,7 +18,7 @@ class CommentForm extends Component{
             isModalOepn:false
         }
       this.toggleModal = this.toggleModal.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this); 
         
   }
 
@@ -136,7 +137,26 @@ class CommentForm extends Component{
     }
 
     const DishDetail=(props)=>{
-     if(props.dish!=null)
+      if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+      }
+      else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+      }
+      
+      else if (props.dish != null)
       return( 
         <div className="container">
           <div className="row">
